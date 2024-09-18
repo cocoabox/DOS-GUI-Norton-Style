9f4aa817347670c80e26bd04fe6f7ba22a59a912
cocoabox <cocoa.kei@gmail.com>
2024-09-19 01:20:06 +0900

    now it builds

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-Doesn't quite work
+![dosbox](images/dosbox.png)
 
+Originally forked from https://github.com/sdancer75/DOS-GUI-Norton-Style/tree/master
+
+This is a framework for showing code-swapping graphical TextUI very similar to that provided by nortonl.lib/stddos.h (see norton utilities 2006 [.7z](https://archive.org/download/SourceCodeCollection/Norton_Utilities_2006_SC.7z)).
+
+## To build
+
+I recommend using DOSBox because it's way easier to edit files shared with DOSBox rather than an MSDOS instance running in VirtualBox.
+
+1. install [turbo assembler](https://github.com/qb40/tasm/) at `\TASM` (drive at your choice)
+2. install [turbo pascal](https://archive.org/details/turbopascal7.0) at `\TP`
+3. chdir to the source root where [build.bat](build.bat) is located
+4. run `build.bat`
+
+## Todo
+
+1. no mouse; WHY?
+
+1. symantec fonts (cpav,norton,norton7.syb) only defines the left/right half of many glyphs, probably due to symmetry; and they are not compatible with this framework
+
+1. the code in oop/*.pas is specifically designed to work with ELINGR.SYB; will have to either fix symantec fonts to work with the code, or have more elaborate skin definitions 
+
+1. background color `WinTextBackGround` greater than 7 is not allowed, unlike NORTONL.LIB which uses bright blue (9) as default background; using anything >7 results in flashing foreground text.
+
+1. many unused unit files needs to be cleaned up
+
+1. better OOP, fewer global variables, indentation and style fix
--- build.bat
+++ build.bat
@@ -2,6 +2,9 @@
 set TA="\TASM\BIN\TASM.EXE"
 set TP="\TP\BIN\TPC.EXE"
 
+if not exist %TA% goto :no_tasm
+if not exist %TP% goto :no_tp
+
 cd obj\
 %TA% -mx asm_ints.asm
 %TA% -mx mousepa.asm
@@ -9,29 +12,40 @@ cd obj\
 %TA% -mx bgi_256.asm
 cd ..\
 
-%TP% -Oobj -Ebin -Ulib  oop\testobj
+set COMPILE_ARG=-Oobj -Ebin -Ulib
+
+%TP% %COMPILE_ARG%  oop\winApplObj
+%TP% %COMPILE_ARG%  oop\errordlg
+%TP% %COMPILE_ARG%  oop\varConst
+%TP% %COMPILE_ARG%  oop\mouseObj
+%TP% %COMPILE_ARG%  oop\generalObj
+%TP% %COMPILE_ARG%  oop\savRstObj
+%TP% %COMPILE_ARG%  oop\windowsO
+%TP% %COMPILE_ARG%  oop\winApplObj
+%TP% %COMPILE_ARG%  oop\pullDnMe
+%TP% %COMPILE_ARG%  oop\topicHlp
+%TP% %COMPILE_ARG%  oop\helpObj
+%TP% %COMPILE_ARG%  oop\winObjA
+%TP% %COMPILE_ARG%  oop\winObjB
+%TP% %COMPILE_ARG%  oop\winObjC
+%TP% %COMPILE_ARG%  oop\demoFx
+%TP% %COMPILE_ARG%  -Ifonts  oop\winInit
+
+:app
+del bin\main.exe
+%TP% %COMPILE_ARG%   -L oop\main
+
+:run
+bin\testobj.exe
 goto :eof
 
-%TP% -Oobj -Ebin -Ulib  oop\errordlg 
-%TP% -Oobj -Ebin -Ulib  oop\varConst
-%TP% -Oobj -Ebin -Ulib  oop\mouseObj
-%TP% -Oobj -Ebin -Ulib  oop\generalObj
-%TP% -Oobj -Ebin -Ulib  oop\savRstObj
-%TP% -Oobj -Ebin -Ulib  oop\windowsO
-%TP% -Oobj -Ebin -Ulib  oop\winApplObj
-%TP% -Oobj -Ebin -Ulib  oop\pullDnMe
-%TP% -Oobj -Ebin -Ulib  oop\topicHlp
-%TP% -Oobj -Ebin -Ulib  oop\helpObj
-%TP% -Oobj -Ebin -Ulib  oop\winObjA
-%TP% -Oobj -Ebin -Ulib  oop\winObjB
-%TP% -Oobj -Ebin -Ulib  oop\winObjC
 
-%TP% -Oobj -Ebin -Ulib  oop\testobj
-%TP% -Oobj -Ebin -Ulib  oop\demoFx
-%TP% -Ifonts -Oobj -Ebin -Ulib  oop\winInit
+:no_tasm
+echo "need tasm %TA% ; if tasm is installed elsewhere please change build.bat"
+goto :eof
 
-%TP% -Oobj -Ebin -Ulib  oop\testobj
-%TP% -Oobj -Ebin -Ulib  oop\test
+:no_tasm
+echo "need turbo pascal %TP% ; if turbo pascal is installed elsewhere please change build.bat"
+goto :eof
 
 :eof
-
